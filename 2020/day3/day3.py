889b3253cd7ef821c99f6e74098f648d4e4376a6
--replace-all <annieb22@mit.edu>
2020-12-13 16:56:31 -0600

    2020 day 3

--- 2020/day3/day3.py
+++ 2020/day3/day3.py
@@ -1,3 +1,5 @@
+from math import prod
+
 filename = '2020/day3/day3.txt'
 puzzle_input = open(filename).readlines()
 
@@ -17,7 +19,8 @@ def part_1():
     return num_trees(3,1)
 
 def part_2():
-    return num_trees(1,1)*num_trees(3,1)*num_trees(5,1)*num_trees(7,1)*num_trees(1,2)
+    all_ways = [(1,1),(3,1),(5,1),(7,1),(1,2)]
+    return prod([num_trees(*way) for way in all_ways])
 
 print("Part 1: {}".format(part_1()))
 print("Part 2: {}".format(part_2()))
