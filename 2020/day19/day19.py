ea834b952d74dc51817beddc44587db719c8f248
--replace-all <annieb22@mit.edu>
2020-12-19 13:47:36 -0600

    2020 day 19

---
+++ 2020/day19/day19.py
@@ -0,0 +1,44 @@
+import regex
+from itertools import count
+
+filename = '2020/day19/day19.txt'
+puzzle_input = open(filename).read()
+
+def process_input():
+    rules, messages = puzzle_input.split('\n\n')
+    new_rules = {}
+    for rule in rules.split('\n'):
+        n, d = rule.split(': ')
+        new_rules[int(n)] = d
+    return new_rules, messages
+
+def build_regex(rules, n, part_2):
+    if part_2:
+        if n == 8:  return f'({build_regex(rules, 42, True)}+)'
+        if n == 11: return f'(?P<{"r0"}>{build_regex(rules, 42, True)}(?P>{"r0"})?{build_regex(rules, 31, True)})'
+
+    rule = rules[n]
+    match = regex.match(r'"(\w)"', rule)
+    if match: return match.group(1)
+    
+    pattern = ""
+    for sub_rule in rule.split(' | '):
+        pattern += "".join([build_regex(rules, int(m), part_2) for m in sub_rule.split()]) + "|"
+    pattern = pattern[:-1] # remove last "|"
+    return f'({pattern})'
+
+def get_messages_that_match(rule, part_2):
+    rules, messages = process_input()
+    return regex.findall(f'^{build_regex(rules, rule, part_2)}$', messages, flags=regex.MULTILINE)
+
+def num_that_match(rule, part_2):
+    return len(get_messages_that_match(rule, part_2))
+
+def part_1():
+    return num_that_match(0, False)
+
+def part_2():
+    return num_that_match(0, True)
+
+print("Part 1: {}".format(part_1()))
+print("Part 2: {}".format(part_2()))
