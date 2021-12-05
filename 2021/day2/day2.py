995beb7bbe6c0e4c5fc40389dd29ae4671669a4f
--replace-all <annieb22@mit.edu>
2021-12-05 17:58:18 -0500

    changed var name:

--- 2021/day2/day2.py
+++ 2021/day2/day2.py
@@ -1,10 +1,10 @@
 day = 2
 filename = f'day{day}/day{day}.txt'
 puzzle_input = open(filename).readlines()
-measurements = [(n.split(' ')[0], int(n.split(' ')[1])) for n in puzzle_input]
+commands = [(n.split(' ')[0], int(n.split(' ')[1])) for n in puzzle_input]
 
 example = [('forward', 5), ('down', 5), ('forward', 8), ('up', 3), ('down', 8), ('forward', 2)]
-# measurements = example
+# commands = example
 
 forward = lambda x: x[0] == 'forward'
 up = lambda x: x[0] == 'up'
@@ -13,15 +13,15 @@ down = lambda x: x[0] == 'down'
 amount = lambda x: x[1]
 
 def part_1():
-  horiz = sum(map(amount, filter(forward, measurements)))
-  depth = sum(map(amount, filter(down, measurements))) - sum(map(amount, filter(up, measurements)))
+  horiz = sum(map(amount, filter(forward, commands)))
+  depth = sum(map(amount, filter(down, commands))) - sum(map(amount, filter(up, commands)))
   return horiz * depth
 
 def part_2():
-  horiz = sum(map(amount, filter(forward, measurements)))
+  horiz = sum(map(amount, filter(forward, commands)))
 
   depth, aim = 0, 0
-  for x in measurements:
+  for x in commands:
     if forward(x):
       depth += aim*amount(x)
     elif down(x):
