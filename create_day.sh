654de4891baba900312dea51a38d2433e0604cca
Annie Bryan <annieb22@mit.edu>
2022-12-03 17:12:17 -0500

    strip rows read in from input file

--- create_day.sh
+++ create_day.sh
@@ -5,7 +5,7 @@ day=$2
 echo "creating files for day ${day} of year ${year}..."
 mkdir ${year}
 mkdir ${year}/day${day}
-echo "day = ${day}\n\nexample_filename = f'day{day}/day{day}_ex.txt'\nexample_input = open(example_filename).readlines()\n\nfilename = f'day{day}/day{day}.txt'\npuzzle_input = open(filename).readlines()\n\ndef part_1(input):\n  return\n\ndef part_2(input):\n  return\n\nprint(f'Part 1 example: {part_1(example_input)}')\nprint(f'Part 1 puzzle: {part_1(puzzle_input)}')\n\nprint(f'Part 2 example: {part_2(example_input)}')\nprint(f'Part 2 puzzle: {part_2(puzzle_input)}')" >> ${year}/day${day}/day${day}.py
+echo "day = ${day}\n\nexample_filename = f'day{day}/day{day}_ex.txt'\nexample_input = [r.strip() for r in open(example_filename).readlines()]\n\nfilename = f'day{day}/day{day}.txt'\npuzzle_input = [r.strip() for r in open(filename).readlines()]\n\ndef part_1(input):\n  return\n\ndef part_2(input):\n  return\n\nprint(f'Part 1 example: {part_1(example_input)}')\nprint(f'Part 1 puzzle: {part_1(puzzle_input)}')\n\nprint(f'Part 2 example: {part_2(example_input)}')\nprint(f'Part 2 puzzle: {part_2(puzzle_input)}')" >> ${year}/day${day}/day${day}.py
 touch ${year}/day${day}/day${day}.txt
 touch ${year}/day${day}/day${day}_ex.txt
 echo "done"
