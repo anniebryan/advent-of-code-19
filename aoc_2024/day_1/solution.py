4b596a8b8012267e370e2fe61b0e634b3e6612d4
Annie Bryan <anniebryan@alum.mit.edu>
2024-12-01 21:53:53 -0500

    rename solve methods

--- aoc_2024/day_1/solution.py
+++ aoc_2024/day_1/solution.py
@@ -3,6 +3,9 @@ Advent of Code 2024
 Day 1: Historian Hysteria
 """
 
+import click
+import os
+import pathlib
 import regex as re
 from collections import Counter
 
@@ -17,7 +20,7 @@ def parse_input(puzzle_input):
     return sorted(left_list), sorted(right_list)
 
 
-def part_1(puzzle_input):
+def solve_part_1(puzzle_input):
     left_list, right_list = parse_input(puzzle_input)
     all_distances = []
     for x, y in zip(left_list, right_list):
@@ -25,10 +28,45 @@ def part_1(puzzle_input):
     return sum(all_distances)
 
 
-def part_2(puzzle_input):
+def solve_part_2(puzzle_input):
     left_list, right_list = parse_input(puzzle_input)
     right_occurrences = Counter(right_list)
     all_distances = []
     for x in left_list:
         all_distances.append(x * right_occurrences[x])
     return sum(all_distances)
+
+
+@click.command()
+@click.option("-se", "--skip_example", is_flag=True, default=False)
+@click.option("-sp", "--skip_puzzle", is_flag=True, default=False)
+def main(skip_example: bool = False, skip_puzzle: bool = False) -> None:
+    base_dir = pathlib.Path(__file__).parent
+    example_files = sorted([fn for fn in os.listdir(base_dir) if fn.endswith(".txt") and "example" in fn])
+
+    def _run_solution(filename: str, display_name: str):
+        print(f"--- {display_name} ---")
+
+        if not (filepath := (base_dir / filename)).exists():
+            print(f"{filename} not found.")
+            return
+
+        with open(filepath) as file:
+            puzzle_input = [line.strip("\n") for line in file]
+            print(f"Part 1: {solve_part_1(puzzle_input)}")
+            print(f"Part 2: {solve_part_2(puzzle_input)}")
+        return
+
+    if not skip_example:
+        if len(example_files) < 2:
+            _run_solution("example.txt", "Example")
+        else:
+            for i, filename in enumerate(example_files):
+                _run_solution(filename, f"Example {i + 1}")
+
+    if not skip_puzzle:
+        _run_solution("puzzle.txt", "Puzzle")
+
+
+if __name__ == "__main__":
+    main()
