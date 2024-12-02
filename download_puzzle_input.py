c5f8e522705cf3553ce54de9dd4d5f43e0c10062
Annie Bryan <anniebryan@alum.mit.edu>
2024-12-02 14:01:55 -0500

    create script to download puzzle input

---
+++ download_puzzle_input.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+import requests
+
+
+def main(year: int, day: int) -> None:
+    with open("config.txt") as config_file:
+        session = config_file.readline().strip()
+    response = requests.get(f"https://adventofcode.com/{year}/day/{day}/input", cookies={"session": session})
+    with open(f"aoc_{year}/day_{day}/puzzle.txt", "w") as puzzle_file:
+        puzzle_file.write(response.text)
+
+
+if __name__ == "__main__":
+    year = int(sys.argv[1])
+    day = int(sys.argv[2])
+    main(year, day)
