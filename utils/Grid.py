7f6700af76f6f9b5b805301034315bd5a4f2d803
Annie Bryan <anniebryan@alum.mit.edu>
2024-12-13 16:49:59 -0500

    add __repr__ to Grid

--- utils/Grid.py
+++ utils/Grid.py
@@ -1,6 +1,8 @@
 class Grid:
     def __init__(self, puzzle_input: list[str]):
         self.values = {}
+        self.width = len(puzzle_input[0])
+        self.height = len(puzzle_input)
         for i, row in enumerate(puzzle_input):
             for j, val in enumerate(row):
                 self.values[(i, j)] = val
@@ -14,3 +16,12 @@ class Grid:
     def __iter__(self):
         for (i, j) in sorted(self.values.keys()):
             yield (i, j)
+
+    def __repr__(self):
+        output = []
+        for i in range(self.height):
+            s = []
+            for j in range(self.width):
+                s.append(str(self.values[(i, j)]))
+            output.append("".join(s))
+        return "\n".join(output)
