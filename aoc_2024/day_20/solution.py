884c82cfde491b700fda8753fc775e6b5413d059
Annie Bryan <anniebryan@alum.mit.edu>
2024-12-23 21:19:36 -0500

    refactor 2024 day 20

--- aoc_2024/day_20/solution.py
+++ aoc_2024/day_20/solution.py
@@ -6,21 +6,53 @@ Day 20: Race Condition
 import click
 import os
 import pathlib
+from typing import Iterable
+from collections import defaultdict, deque
 from utils import Grid
 
 
-def get_cheat_candidates(g: Grid) -> set[tuple[tuple[int, int], tuple[int, int]]]:
+def neighbors(g: Grid, loc: tuple[int, int]) -> Iterable[tuple[int, int]]:
+    i, j = loc
+    for ni, nj in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+        if g.in_bounds(ni, nj):
+            yield (ni, nj)
+
+
+def bfs(g: Grid, start: tuple[int, int], cheat_len: int) -> Iterable[tuple[int, int]]:
+    """Yields locs (i, j) where `g.at(i, j)` is either "." or "E" which can be reached from `start`
+    in at most `cheat_len` steps through a path consisting of only '#' spaces."""
+    q = deque([(start, cheat_len)])
+    seen = set()
+    while q:
+        (curr, l) = q.popleft()
+        for (ni, nj) in neighbors(g, curr):
+            if (ni, nj) not in seen:
+                seen.add((ni, nj))
+                if g.at(ni, nj) in ".E":
+                    yield (ni, nj)
+                if l > 1 and g.at(ni, nj) == "#":
+                    q.append(((ni, nj), cheat_len - 1))
+
+
+def get_cheat_candidates(g: Grid, cheat_len: int) -> set[tuple[tuple[int, int], tuple[int, int]]]:
     cands = set()
-    for (s_i, s_j) in g.where(".") + g.where("S"):
-        for (n_i, n_j) in [(s_i + 1, s_j), (s_i - 1, s_j), (s_i, s_j + 1), (s_i, s_j - 1)]:
-            if g.in_bounds(n_i, n_j) and g.at(n_i, n_j) == "#":
-                for (m_i, m_j) in [(n_i + 1, n_j), (n_i - 1, n_j), (n_i, n_j + 1), (n_i, n_j - 1)]:
-                    if (m_i, m_j) != (s_i, s_j) and g.in_bounds(m_i, m_j) and g.at(m_i, m_j) in ".E":
-                        cands.add(((s_i, s_j), (m_i, m_j)))
+    for start in g.where(".") + g.where("S"):
+        for end in bfs(g, start, cheat_len):
+            cands.add((start, end))
     return cands
 
 
-def solve_part_1(puzzle_input: list[str]):
+def display_res(time_saved_dict: dict[int, int]) -> None:
+    for t in sorted(time_saved_dict):
+        num_cheats = time_saved_dict[t]
+        if t > 0:
+            if num_cheats > 1:
+                print(f"There are {num_cheats} cheats that save {t} picoseconds.")
+            elif num_cheats == 1:
+                print(f"There is one cheat that saves {t} picoseconds.")
+
+
+def num_cheats(puzzle_input: list[str], cheat_len: int, picoseconds_saved: int) -> int:
     g = Grid(puzzle_input)
 
     start = g.where("S")[0]
@@ -28,15 +60,20 @@ def solve_part_1(puzzle_input: list[str]):
     dists_from_start = g.dijkstra(start)
     dists_from_end = g.dijkstra(end)
     len_without_cheating = dists_from_start[end]
+    cheat_candidates = get_cheat_candidates(g, cheat_len)
 
-    tot = 0
-    cheat_candidates = get_cheat_candidates(g)
+    time_saved_dict = defaultdict(int)
     for (c_s, c_e) in cheat_candidates:
         if dists_from_start[c_s] < dists_from_start[c_e]:
             dist_with_cheat = dists_from_start[c_s] + 2 + dists_from_end[c_e]
-            if len_without_cheating - dist_with_cheat >= 100:
-                tot += 1
-    return tot
+            time_saved = len_without_cheating - dist_with_cheat
+            time_saved_dict[time_saved] += 1
+
+    return sum([v for k, v in time_saved_dict.items() if k >= picoseconds_saved])
+
+
+def solve_part_1(puzzle_input: list[str]):
+    return num_cheats(puzzle_input, 2, 100)
 
 
 # TODO
